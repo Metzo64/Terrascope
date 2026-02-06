ba8b17913be17ebb7146eddec09761000c6c57db
Maitreya Pawar <140409152+Metzo64@users.noreply.github.com>
2026-02-06 20:23:07 +0530

    Add files via upload

--- Terrascope frontend/app.py
+++ Terrascope frontend/app.py
@@ -5,5 +5,5 @@ PORT = 5000
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving TerraScope at http://localhost:{PORT}")
+    print(f"Serving TerraScope at http://localhost:{PORT}/signup.html")
     httpd.serve_forever()
